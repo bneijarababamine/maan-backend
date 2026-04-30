6c24a2689ccce63285cb7c19845a5944c8915f6b
B'Neijara baba mine <135664616+bneijarababamine@users.noreply.github.com>
2026-04-30 18:51:21 +0000

    fix: seeder only creates admin users, no test data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,23 +32,7 @@ class DatabaseSeeder extends Seeder
             ]
         );
 
-        // Données de test
-        $this->call([
-            MemberSeeder::class,
-            DonorSeeder::class,
-            DonationSeeder::class,
-            OrphanSeeder::class,
-            FamilySeeder::class,
-            ActivitySeeder::class,
-        ]);
-
         $this->command->info('');
         $this->command->info('✓ Utilisateurs  : 36363833 / 36064707');
-        $this->command->info('✓ Membres       : 15 membres créés');
-        $this->command->info('✓ Donateurs     : 12 donateurs créés');
-        $this->command->info('✓ Dons          : 20 dons créés');
-        $this->command->info('✓ Orphelins     : 18 orphelins créés (avec fratries)');
-        $this->command->info('✓ Familles      : 10 familles créées');
-        $this->command->info('✓ Activités     : 6 activités avec bénéficiaires');
     }
 }
